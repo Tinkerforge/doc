6aaca926cbd9b46bd9ce4b24988272b07a3ea03b
Bastian Nordmeyer <bastian@tinkerforge.com>
2014-06-05 16:57:09 +0200

    remove obsolete build files

--- de/fastbuid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ $1 ]
-  then if [ $2 ]
-    then sphinx-build -b html -c source/ -D master_doc=$1 $2 build/fasthtml
-  else echo "Usage: ./fastbuild.sh MasterFile source/Directory/To/Build"
-  fi
-else echo "Usage: ./fastbuild.sh MasterFile source/Directory/To/Build"
-fi
