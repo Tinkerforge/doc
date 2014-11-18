707e257ec8a43d3534bfb85ced1ba59329dd577e
Olaf Lüke <olaf@tinkerforge.com>
2014-11-18 17:57:09 +0100

    Add example code for tutorial

---
+++ en/source/Tutorials/Tutorial_RED_Brick/index.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+
+from flask import Flask       # Use Flask framework
+application = Flask(__name__) # Function "application" is used by Apache/wsgi
+app = application             # Use shortcut for routing
+
+HOST = "localhost"
+PORT = 4223
+UID = "qaQ" # Change to your UID
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_temperature import Temperature
+
+PAGE = u"""
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html>
+ <head>
+  <title>Temperature</title>
+ </head>
+ <body>
+  <p>Temperature: {0} °C<p>
+ </body>
+</html>
+"""
+
+@app.route('/')
+def index():
+    ipcon = IPConnection() # Create IP connection
+    t = Temperature(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
+
+    # Get current temperature (unit is °C/100)
+    temperature = t.get_temperature()/100.0
+
+    ipcon.disconnect()
+    return PAGE.format(temperature)
